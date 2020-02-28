828d7cd5282e9798af994380280f06a9a25502de
枫叶思源 <britton@126.com>
2020-02-28 16:09:04 +0800

    修复默认缓存的问题

--- src/main/java/com/geoxus/ethereum/service/impl/MarketInformationServiceImpl.java
+++ src/main/java/com/geoxus/ethereum/service/impl/MarketInformationServiceImpl.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +18,12 @@ public class MarketInformationServiceImpl implements MarketInformationService {
     private static final String URL = "http://api.coindog.com/api/v1/currency/ranks";
 
     @Override
-    @Cacheable(value = "marketInfo", key = "#params.hashCode()")
+    @Cacheable(value = "market-info", key = "#params.hashCode()")
     public List<Map<String, Object>> marketInformation(List<String> params) {
         final byte[] bytes = HttpRequest.get(URL).execute().bodyBytes();
         final List<Map<String, Object>> retData = new ArrayList<>();
         try {
-            final String s = new String(bytes, Charset.forName("UTF-8"));
+            final String s = new String(bytes, StandardCharsets.UTF_8);
             final List<Map<String, Object>> lists = JSONUtil.toBean(s, List.class);
             for (int i = 0; i < params.size(); i++) {
                 for (Map<String, Object> map : lists) {
